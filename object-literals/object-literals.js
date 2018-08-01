65b42eb66fb1f926c8bc08e7f470b984410823c7
omer19-meet <omer19@meet.mit.edu>
2018-08-01 07:09:58 -0400

    nice work

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,12 @@
 // Create your 'me' object literal here!
+var me = {
+	firstName: " Omer",
+	lastName:  " Ichar",
+	age: 16 ,
+	favoriteFoods: [ "Passta", "Cobe", " kabab"
+	],
+	picture: "https://secure.i.telegraph.co.uk/multimedia/archive/03597/POTD_chick_3597497k.jpg",
+
+}
+
+document.write(JSON.stringify(me));
