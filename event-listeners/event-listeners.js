bbdcdac8bd8b68f3054822d33645a06177bff052
omer19-meet <omer19@meet.mit.edu>
2018-08-02 09:25:44 -0400

    secssfull lab

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,16 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['./imgs/dog.jpg' , './imgs/dead.jpeg', './imgs/free-fly.jpeg', './imgs/do-kanfy.jpeg' , './imgs/pailot-cabin.jpeg', './imgs/pasnger-window.jpeg', './imgs/window.jpeg'] ; 
 var currentIndex = 0;
-
+document.getElementsByTagName('img')[0].addEventListener("click", showNextPicture)
 function showNextPicture() {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
   }
+  var pic  = document.getElementsByTagName('img')
+  pic[0].src = pictures[currentIndex];
+}
 
   // YOUR TASK: Finish this function!
-}
+
--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,10 @@
-// Put all your JavaScript in this file!
+// Put all your JavaScript in this file
+function ask_color(ans) {
+	var ans = prompt("what color do you want to see this website?");
+	var body = document.getElementsByTagName('body');
+
+	body[0].style.backgroundColor = ans
+
+
+	// body...
+}
